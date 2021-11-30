07412905f64783a01cc86a70729c40a03cf688ec
andreaferraboli <57558228+andreaferraboli@users.noreply.github.com>
2021-11-30 10:46:47 +0100

    add buycar.php ,modify one row of buycar.html and sellcar.html

--- php/buycar.php
+++ php/buycar.php
@@ -2,21 +2,16 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', '', '','macchine_tpi');
+$con = mysqli_connect('localhost', 'root', '','macchine_tpi');
 
 // get the post records
-//$marca = $_POST['marca'];
-//$modello = $_POST['modello'];
-//$condizione = $_POST['condizione'];
-//$kilometraggio = $_POST['kilometraggio'];
-//$cavalli = $_POST['cavalli'];
-//$prezzo = $_POST['prezzo'];
-$marca = "Audi";
-$modello = "Q8";
-$condizione = "usata";
-$kilometraggio = 10000;
-$cavalli = 320;
-$prezzo = 100000;
+$marca = $_POST['marca'];
+$modello = $_POST['modello'];
+$condizione = $_POST['condizione'];
+$kilometraggio = $_POST['kilometraggio'];
+$cavalli = $_POST['cavalli'];
+$prezzo = $_POST['prezzo'];
+
 // database insert SQL code
 $sql = "INSERT INTO Macchine (marca,modello,condizione,kilometraggio,cavalli,prezzo) VALUES ($marca,$modello,$condizione,$kilometraggio,$cavalli,$prezzo)";
 
@@ -26,9 +21,6 @@ $rs = mysqli_query($con, $sql);
 if($rs)
 {
        echo "Contact Records Inserted";
-}
-else{
-    echo "connessione non avvenuta";
 }
 
 ?>
