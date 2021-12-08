77fb552cfc0ec0de34298cd3a5130b9be0f159ef
Andrea Ferraboli <andreaferraboli2004@gmail.com>
2021-12-08 15:52:53 +0100

    add 3 layout column in buycar.php

--- php/buycar.php
+++ php/buycar.php
@@ -1,29 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-        <meta name="generator" content="Hugo 0.88.1">
+    <meta charset="utf-8">
 
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
-
-
-
-        <!-- Bootstrap core CSS -->
-        <link href="/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-        <!-- Favicons -->
-
-        <meta name="theme-color" content="#7952b3">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/buycar.css">
 </head>
 <body>
@@ -47,25 +26,28 @@ if ($conn->connect_error) {
 
     if ($result->num_rows > 0) {
 // output data of each row
-        $numberElements=0;
-        echo '<div class="container">';
+        $numberElements = 0;
+        echo '<div class="containerCard">';
         while ($row = $result->fetch_assoc()) {
             $nameCar = $row["brand"] . "-" . $row["modello"];
-            if ($numberElements % 3==0)
-                echo '<div class="row ">';
-            echo '<div class="carCard">';
-            echo "<h1>" . $nameCar . "</h1>";
+            if ($numberElements % 3 == 0)
+                echo '<div class="row">';
+            echo '<div class="column">';
+            echo '<div class="card">';
+            echo '<h1 class="cardH1">' . $nameCar . "</h1>";
             echo '<img src="car/' . $row["modello"] . '.jpg" alt="' . $nameCar . '" style="width:100%">';
             echo '<p class="price">' . $row["prezzo"] . " €</p>";
             echo '<p class="information">condizione:' . $row["condizione"] . "</p>";
             echo '<p class="information">cavalli:' . $row["cavalli"] . "</p>";
             echo '<p class="information">kilometraggio:' . $row["kilometraggio"] . "</p>";
-            echo '<button class="btn-primary btn" onclick="">Compra Macchina</button>';
+            echo '<button class="cardButton" >Compra Macchina</button>';
             echo "</div>";
-            if ($numberElements % 3==2)
+            echo "</div>";
+            if ($numberElements % 3 == 2)
                 echo "</div>";
             $numberElements++;
         }
+        echo "</div>";
         echo "</div>";
     } else {
         echo "0 results";
