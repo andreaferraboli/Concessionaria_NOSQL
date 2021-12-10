101021c199697b1741bf65a585e778842e825586
Andrea Ferraboli <andreaferraboli2004@gmail.com>
2021-12-10 02:46:28 +0100

    first version of the project

--- php/boughtCar.php
+++ php/boughtCar.php
@@ -3,52 +3,65 @@
 <head>
     <meta charset="utf-8">
 
-    <link rel="stylesheet" href="../css/buycar.css">
+    <link rel="stylesheet" href="../css/boughtcar.css">
 </head>
 <body>
 
 <h3 class="title">Auto Comprata</h3>
 <?php
-$id = array_key_exists('id1', $_GET) ? $_GET['id1'] : 5;
-$brand = array_key_exists('brand1', $_GET) ? $_GET['brand1'] : '';
-$modello = array_key_exists('modello1', $_GET) ? $_GET['modello1'] : '';
-$condizione = array_key_exists('condizione1', $_GET) ? $_GET['condizione1'] : '';
-$kilometraggio = array_key_exists('kilometraggio1', $_GET) ? $_GET['kilometraggio1'] : '';
-$cavalli = array_key_exists('cavalli1', $_GET) ? $_GET['cavalli1'] : '';
-$prezzo = array_key_exists('prezzo1', $_GET) ? $_GET['prezzo1'] : '';
-$conn = new mysqli('localhost', 'root', '', 'macchine_tpi');
-if ($conn->connect_error) {
-    echo "$conn->connect_error";
-    die("Connection Failed : " . $conn->connect_error);
-} else {
-    $sql = "DELETE FROM macchine WHERE id_macchina=".$id;
-    if (mysqli_query($conn, $sql)) {
-        echo "Record deleted successfully";
-        echo '<div class="containerCard">';
-        $nameCar = $brand . "-" . $modello;
-        echo '<div class="column">';
-        echo '<div class="card">';
-        echo '<h1 class="cardH1">' . $nameCar . "</h1>";
-        echo '<img src="car/' . $modello . '.jpg" alt="' . $nameCar . '" style="width:100%">';
-        echo '<p class="price">' . $prezzo . " €</p>";
-        echo '<p class="information">condizione:' . $condizione . "</p>";
-        echo '<p class="information">cavalli:' . $cavalli . "</p>";
-        echo '<p class="information">kilometraggio:' . $kilometraggio . "</p>";
-        echo '<button class="cardButton" >Macchina Comprata!</button>';
-        echo "</div>";
-        echo "</div>";
+if (isset($_POST['submit2'])) {
+    $id = $_POST['id_macchina'];
+    $conn = new mysqli('localhost', 'root', '', 'macchine_tpi');
+    $sql = "SELECT * FROM macchine where macchine.id_macchina=" . $id;
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
 
+        while ($row = $result->fetch_assoc()) {
+            $brand = $row["brand"];
+            $modello = $row["modello"];
+            $condizione = $row["condizione"];
+            $kilometraggio = $row["kilometraggio"];
+            $cavalli = $row["cavalli"];
+            $prezzo = $row["prezzo"];
+        }
     } else {
-        echo "Error deleting record: " . $conn->error;
+        echo "id non trovato";
     }
-}
+    if ($conn->connect_error) {
+        echo "$conn->connect_error";
+        die("Connection Failed : " . $conn->connect_error);
+    } else {
+        $sql = "DELETE FROM macchine WHERE id_macchina=" . $id;
+        if (mysqli_query($conn, $sql)) {
+            echo '<div class="containerCard">';
+            $nameCar = $brand . "-" . $modello;
+            echo '<div class="column">';
+            echo '<div class="card">';
+            echo '<h1 class="cardH1">' . $nameCar . "</h1>";
+            echo '<img src="car/' . $modello . '.jpg" alt="' . $nameCar . '" style="width:100%">';
+            echo '<p class="price">' . $prezzo . " €</p>";
+            echo '<p class="information">condizione:' . $condizione . "</p>";
+            echo '<p class="information">cavalli:' . $cavalli . "</p>";
+            echo '<p class="information">kilometraggio:' . $kilometraggio . "</p>";
+            echo '<button class="cardButton" >Macchina Comprata!</button>';
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
 
+        } else {
+            echo "Error deleting record: " . $conn->error;
+        }
+    }
+} else {
+    echo "you don't have submited form 2";
+}
 ?>
 
 <footer class="footer">
     <a href="../index.html" TARGET="_self" class="link-footer">home</a>
     <a href="../sellcar.html" TARGET="_self" class="link-footer">Vendi Auto</a>
     <a href="../buycar.html" TARGET="_self" class="link-footer">Compra Auto</a>
+    <a href="../updatecar.html" TARGET="_self" class="link-footer">Aggiorna Auto</a>
 </footer>
 </body>
 </html>
--- php/buycar.php
+++ php/buycar.php
@@ -9,6 +9,12 @@
 
 <h3 class="title">Compra la tua auto!</h3>
 <?php
+$default = [
+    'id_macchina' => '',
+    // Qui aggiungerai tutti gli altri valori di default
+];
+
+$_POST = array_replace( $default, $_POST );
 $brand = $_POST['brand'];
 $modello = $_POST['modello'];
 $condizione = $_POST['condizione'];
@@ -35,16 +41,18 @@ if ($conn->connect_error) {
                 echo '<div class="row">';
             echo '<div class="column">';
             echo '<div class="card">';
-            echo '<form method="get" action="boughtCar.php" >';
-            echo '<h1 class="cardH1" name="id1" id="id1">' . $row["id_macchina"] . "</h1>";
-            echo '<h1 class="cardH1" name="brand1" id="brand1">' . $row["brand"] . "</h1>";
-            echo '<h1 class="cardH1" name="modello1" id="modello1">' . $row["modello"] . "</h1>";
+            echo '<form name="buyform" method="post" action="boughtCar.php">';
+            echo '<select id="id_macchina" name="id_macchina" >';
+            echo '<option value="'.$row["id_macchina"].'" selected>'.$row["id_macchina"]."</option>";
+            echo'</select>';
+            echo '<h1 class="cardH1" >' . $row["brand"] . "</h1>";
+            echo '<h1 class="cardH1">' . $row["modello"] . "</h1>";
             echo '<img src="car/' . $row["modello"] . '.jpg" alt="' . $nameCar . '" style="width:100%">';
-            echo '<p class="price" name="prezzo1" id="prezzo1">' . $row["prezzo"] . " €</p>";
-            echo '<p class="information" name="condizione1" id="condizione1">condizione:' . $row["condizione"] . "</p>";
-            echo '<p class="information" name="cavalli1" id="cavalli1">cavalli:' . $row["cavalli"] . "</p>";
-            echo '<p class="information" name="kilometraggio1" id="kilometraggio1">kilometraggio:' . $row["kilometraggio"] . "</p>";
-            echo '<input type="submit" value="Compra Macchina" class="cardButton" ></input>';
+            echo '<p class="price" >' . $row["prezzo"] . " €</p>";
+            echo '<p class="information" >condizione:' . $row["condizione"] . "</p>";
+            echo '<p class="information" >cavalli:' . $row["cavalli"] . "</p>";
+            echo '<p class="information" >kilometraggio:' . $row["kilometraggio"] . "</p>";
+            echo '<input type="submit" value="Compra Macchina" class="cardButton" name="submit2" >'.'</input>';
 //            echo '<button class="cardButton" />Compra Macchina</button>';
             echo "</form>";
             echo "</div>";
@@ -107,11 +115,16 @@ function createQuery($brand, $modello, $condizione, $kilometraggio, $cavalli, $p
 }
 
 ?>
-
+<!--<script type="text/javascript">-->
+<!--    function setId(id){-->
+<!--        document.cookie = "id="+id++ "; path=/; domain=..";-->
+<!--    }-->
+<!--</script>-->
 <footer class="footer">
     <a href="../index.html" TARGET="_self" class="link-footer">home</a>
     <a href="../sellcar.html" TARGET="_self" class="link-footer">Vendi Auto</a>
     <a href="../buycar.html" TARGET="_self" class="link-footer">Compra Auto</a>
+    <a href="../updatecar.html" TARGET="_self" class="link-footer">Aggiorna Auto</a>
 </footer>
 </body>
 </html>
